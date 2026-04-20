eceefa986b391c66b882d5963ea452925c56d385
pedrovfpprogrram <pedrovfpprogram@yahoo.com>
2026-04-20 20:12:36 -0300

    Aula 06

---
+++ aula06/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int n1,n2; //Variáveis Globais
+
+int main(){
+
+    //Operadores Matemáticos: + - / * % ()
+    //% resto divisão
+    
+    int n3,n4; //Variáveis Locais
+    int res1,res2;
+
+    n1=11;
+    n2=3;
+    n3=5;
+    n4=2;
+
+    res1=n1/n2;
+    res2=n1%n2;
+
+    cout << "Divisao:" << res1 << "\n\n";
+    cout << "Resto: " << res2 << "\n\n";
+
+    return 0;
+}
