520fdf16f6c33d50188bdfd6c43599ddf65bcb45
pedrovfpprogrram <pedrovfpprogram@yahoo.com>
2026-04-20 19:54:36 -0300

    Aula 05

---
+++ aula05/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.1415
+#define canal cout << "Pedro\n\n";
+
+int main(){
+
+    int vidas=3, tiros=500, life=100;
+
+    cout << pi << "\n\n";
+    canal
+
+    return 0;
+}
