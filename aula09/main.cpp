b378b2606e1a63e122c041781962f0a9aca4fd0d
pedrovfpprogrram <pedrovfpprogram@yahoo.com>
2026-04-21 12:06:45 -0300

    Aula 09

---
+++ aula09/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1 = 10;
+    int num2 = 50; 
+    int sol = 5;
+    char opc = 's';
+
+    if(sol >= 10){
+        cout << "Vou ao clube";
+    }else{
+        cout << "Vou ao cinema";
+    }
+
+
+
+    return 0;
+
+}
